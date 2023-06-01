c794c6b4aa538a5886c594f5e6720775ee1890d2
naitelkadir <naitelkadir@gmail.com>
2023-06-01 15:39:10 -0700

    test

--- factors.py
+++ factors.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
-factorization = __import__('task0').factorization
-factorization(1718944270642558716715)
+import sys
+
+
+def factorize(number):
+    factors = []
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            factors.append((i, number // i))
+            return factors
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        return
+
+    input_file = sys.argv[1]
+    try:
+        with open(input_file, 'r') as file:
+            for line in file:
+                number = int(line.strip())
+                factor_pairs = factorize(number)
+                for p, q in factor_pairs:
+                    print(f"{number}={p}*{q}")
+    except FileNotFoundError:
+        print(f"File '{input_file}' not found.")
+
+
+if __name__ == '__main__':
+    main()
+
--- test.py
+++ test.py
@@ -2,9 +2,9 @@
 """ define a function to factorize natural numbers"""
 def factorization(n):
     if n % 2 == 0:
-        print("{} = {} * {}".format(n, n // 2, 2))
+        print("{} = {} * {}".format(n, n / 2, 2))
     else:
         for i in range(3, 10):
             if n % i == 0:
-                print("{} = {} * {}".format(n, i, n // i))
+                print("{} = {} * {}".format(n, i, n / i))
                 break
